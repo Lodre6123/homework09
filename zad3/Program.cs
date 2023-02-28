de95aae4f50e6b051f96c1fc6c2bc02b70100041
Lodre6123 <nikpash6123@mail.ru>
2023-02-28 11:01:58 +0300

    za3

---
+++ zad3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+int m = InputInt("Введем число m:");
+int n = InputInt("Введем число n:");
+Console.WriteLine($"A({m}, {n}) = {Akerman(m, n)}");
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+int Akerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Akerman(m - 1, 1);
+    else
+        return Akerman(m - 1, Akerman(m, n - 1));
+}
+
