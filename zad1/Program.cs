61a55673e99821413a4bb5c972ab66621d069386
Lodre6123 <nikpash6123@mail.ru>
2023-02-28 10:49:58 +0300

    z1

---
+++ zad1/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/ 
+
+void AllNaturals (int max)
+{
+    if (max > 1) 
+    {
+        Console.Write(max + ", ");
+        AllNaturals(max - 1);
+    }
+    else 
+        Console.WriteLine("Ошибка");
+    
+}
+
+Console.Write("Введите натуральное число: ");
+AllNaturals(Convert.ToInt32(Console.ReadLine())); 
